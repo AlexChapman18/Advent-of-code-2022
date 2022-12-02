098812dd054fd7e02c92553a592c51e60f590dda
Alexander Chapman <achapman5@sheffield.ac.uk>
2022-12-02 00:17:54 +0000

    Finished day one :)

--- Day 1/Day1.hs
+++ Day 1/Day1.hs
@@ -1,37 +1,32 @@
 import System.IO
 import Control.Monad
+import Data.List
 
-
-intakeFile :: FilePath -> Int -> IO [Int]
-intakeFile filePath n = fmap (getMostCalories 3) formattedValues
+-- Reads in the file
+intakeFile :: FilePath -> Int -> IO Int
+intakeFile filePath arg1 = mainMethodOutput
   where
-    formattedValues = fmap (lines) (readFile filePath)
+    mainMethodOutput = fmap (mainMethod arg1) integerLines
+    integerLines = fmap (map stringToInt) rawLines
+    rawLines = fmap (lines) (readFile filePath)
 
-getMostCalories :: Int -> [String] -> [Int]
-getMostCalories n inputList = getTopNValues formattedValues n
-  where
-    formattedValues = sumSections 0 formattedInput
-    formattedInput = map (stringToInt) inputList
-
-getTopNValues :: [Int] -> Int -> [Int]
-getTopNValues values 0 = []
-getTopNValues values n = maximumA values : getTopNValues (removeValue values (maximumA values)) (n-1)
-
-sumSections :: Int -> [Int] -> [Int]
-sumSections currentTotal [] = [currentTotal]
-sumSections currentTotal (-1:rest) = currentTotal : (sumSections 0 rest)
-sumSections currentTotal (top:rest) = sumSections (currentTotal + top) rest
-
-maximumA :: [Int] -> Int
-maximumA [] = 0
-maximumA (x:xs) | (maximumA xs) > x = (maximumA xs)
-                | otherwise = x
-
-removeValue :: [Int] -> Int -> [Int]
-removeValue [] value = []
-removeValue (x:xs) value | x == value = 0 : xs
-                         | otherwise = x : removeValue xs value
-
+-- Converts the strings to ints
 stringToInt :: String -> Int
 stringToInt "" = -1
 stringToInt value = read value :: Int
+
+
+-- Main method
+mainMethod :: Int -> [Int] -> Int
+mainMethod n rawData = sum topNValues
+  where
+    topNValues = take n sortedValues
+    sortedValues = (reverse.sort) summedValues
+    summedValues = sumGroups 0 rawData
+
+
+--Sums each group of calories
+sumGroups :: Int -> [Int] -> [Int]
+sumGroups total [] = [total]
+sumGroups total (-1:tail) = total : sumGroups 0 tail
+sumGroups total (head:tail) = sumGroups (total + head) tail
